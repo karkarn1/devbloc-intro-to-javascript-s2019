023ceb97d70d495e4fa72c096dd28f1158fd26a8
Karim Abou-Khalil <karim.abou-khalil@paladin.ai>
2019-05-27 19:58:23 -0400

    Add workshop

---
+++ workshops/week-3/lecture-1/changeElementContent/scripts.js
@@ -0,0 +1,6 @@
+function changeElementContent(id, content) {
+    document.getElementById(id).innerText=content;
+}
+changeElementContent("first_el","title");
+changeElementContent("second_el", "my content");
+changeElementContent("third_el", "a sub heading");
